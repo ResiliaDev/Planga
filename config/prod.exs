24d2185014259b07c41e96abaf68427b62459704
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-06-01 14:11:56 +0200

    check_origin: false

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,8 @@ config :plange, PlangeWeb.Endpoint,
   server: true,
   root: ".",
   version: Mix.Project.config[:version],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  check_origin: false # Allow socket/channel connections from anywhere
 
 # Do not print debug messages in production
 config :logger, level: :info
