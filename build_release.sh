5011fbe824602b3349dab27f9170ae44a371dd97
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-06-01 11:22:28 +0200

    fix build_release.sh script

--- build_release.sh
+++ build_release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./node_modules/brunch/bin/brunch b -p && MIX_ENV=prod mix do phoenix.digest, release --env=prod --upgrade
+cd assets && ./node_modules/brunch/bin/brunch b -p && cd .. && MIX_ENV=prod mix do phoenix.digest, release --env=prod --upgrade
--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,14 @@ use Mix.Config
 # which you typically run after static files are built.
 config :plange, PlangeWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  # url: [host: "example.com", port: 80],
+  # cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}],
+  url: [host: "localhost", port: {:system, "PORT"}],
+  server: true,
+  root: ".",
+  version: Mix.Project.config[:version],
+  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
