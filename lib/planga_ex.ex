7995df92b98bb8a367d8f8e45fb60ce81cc6aa18
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-08-16 12:05:21 +0200

    current user in suboption

--- lib/planga_ex.ex
+++ lib/planga_ex.ex
@@ -11,7 +11,7 @@ defmodule PlangaPhoenix do
     {:ok, options} = validate_options(options)
 
 
-    encrypted_info = encrypted_conversation_info(options.private_api_key, options.conversation_id, options.current_user_id, options.current_user_name)
+    encrypted_info = encrypted_conversation_info(options.private_api_key, options.conversation_id, options.current_user)
 
     options =
       options
@@ -36,14 +36,14 @@ defmodule PlangaPhoenix do
     ]
   end
 
-  defp encrypted_conversation_info(private_api_key, conversation_id, current_user_id, current_user_name) do
+  defp encrypted_conversation_info(private_api_key, conversation_id, current_user = %{id: _, name: _}) do
     decoded_privkey = JOSE.JWK.from_map(%{"k" => private_api_key, "kty" => "oct"})
     inspect decoded_privkey
     priv_data =
       %{
         conversation_id: conversation_id,
-        current_user_id: current_user_id,
-        current_user_name: current_user_name
+        current_user_id: current_user.id,
+        current_user_name: current_user.name
       } |> Poison.encode!
 
     decoded_privkey
@@ -53,17 +53,20 @@ defmodule PlangaPhoenix do
   end
 
   defp validate_options(options) do
-    errors = Vex.errors(options, [
-          public_api_id: [presence: true, format: ~r/[a-zA-Z]+/],
-          private_api_key: [presence: true, format: ~r/[a-zA-Z]+/],
-          conversation_id: [presence: true],
-          current_user_id: [presence: true],
-          current_user_name: [presence: true],
-        ])
+    errors = Vex.errors(options, %{
+          :public_api_id => [presence: true, format: ~r/[a-zA-Z]+/],
+          :private_api_key => [presence: true, format: ~r/[a-zA-Z]+/],
+          :conversation_id => [presence: true],
+          :current_user => [presence: true],
+          # [:current_user, :id] => [presence: true],
+          # [:current_user, :name] => [presence: true],
+        })
 
-    if(errors != []) do
+
+    if errors != [] do
       {:error, errors}
     else
+      errors = Vex.errors(options.current_user, %{id: [presence: true], name: [presence: true]})
       {:ok, options}
     end
   end
