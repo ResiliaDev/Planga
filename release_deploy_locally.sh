b4670632313d63bc1c7687b4ca8e9d98584d1a8d
bernie <bernie@vera.colo.transip.net>
2018-08-29 14:50:29 +0200

    fix local deployment script

--- release_deploy_locally.sh
+++ release_deploy_locally.sh
@@ -9,9 +9,9 @@ MIX_ENV=prod mix do phoenix.digest, release --env=prod --upgrade
 echo "Done!"
 echo "Continuing to upload release to local server..."
 echo "Enter your sudo-password to copy-over the application and hot-upgrade the application to ${relnum}."
-sudo rsync -hvrPt --ignore-existing _build/prod/rel/planga/releases/${relnum}/planga.tar.gz /var/www/planga/releases/${relnum}/
+sudo rsync -hvrPt --ignore-existing _build/prod/rel/planga/releases/${relnum}/planga.tar.gz /var/www/plange/releases/${relnum}/
 echo "Done Copying!"
 echo "Attempting to upgrade application through SSH..."
-sudo /var/www/planga/bin/planga upgrade ${relnum}
+sudo /var/www/planga/bin/plange upgrade ${relnum}
 echo "Done!"
 echo "Deploy finished!"
