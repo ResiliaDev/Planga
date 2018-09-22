a9e704743476030bb9ff70dfd32447314440ce58
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-09-23 01:04:01 +0200

    bump  version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Planga.Mixfile do
   def project do
     [
       app: :planga,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
