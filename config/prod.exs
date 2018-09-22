82cd488b3d1c795d8688ee5c95fe1abee89ff4b6
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-09-23 01:03:30 +0200

    HTTPS urls

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :planga, PlangaWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "chat.planga.io", port: 80],
+  url: [host: "chat.planga.io"],
   # cache_static_manifest: "priv/static/cache_manifest.json"
   http: [port: {:system, "PORT"}],
   # url: [host: "localhost", port: {:system, "PORT"}],
