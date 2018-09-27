989ad17c97df0f59b9c73969898c2c0daea113a7
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-09-27 15:29:11 +0200

    Sync active flag when syncing API key pairs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Planga.Mixfile do
   def project do
     [
       app: :planga,
-      version: "0.3.8",
+      version: "0.3.9",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
