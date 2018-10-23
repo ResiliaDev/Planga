dc824b7b6a965ed88c01501c738dd3d5dd24163b
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-10-23 16:26:00 +0200

    Adds cold release shell script

---
+++ cold_release_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mix deps.get
+mix compile # Required to have the following command not contain warnings.
+relnum=`mix app_version`
+echo "Creating COLD release for version ${relnum}..."
+cd ./assets;
+./node_modules/brunch/bin/brunch b -p ;
+cd ../;
+MIX_ENV=prod mix do phoenix.digest, release --env=prod
+echo "Rsyncing COLD release to Planga directory..."
+sudo rsync -hvrPt --ignore-existing _build/prod/rel/planga/${relnum}/planga.tar.gz /var/www/planga/
+sudo service planga restart
