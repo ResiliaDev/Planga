8177d2f3db3402d5b818af3ba018bb94a4f0ada8
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-11-03 23:59:29 +0100

    Make the Elixir listener cope with RabbitMQ failures

--- lib/planga/app_settings_listener.ex
+++ lib/planga/app_settings_listener.ex
@@ -12,12 +12,36 @@ defmodule Planga.AppSettingsListener do
   @exchange_name "planga_app_settings_updates"
 
   def init(_opts) do
-    {:ok, conn} = AMQP.Connection.open() # TODO Configure
-    {:ok, channel} = AMQP.Channel.open(conn)
+    rabbitmq_connect()
+  end
 
+  defp rabbitmq_connect do
+    case AMQP.Connection.open() do # TODO Configure location
+      {:ok, conn} ->
+        # Be notified when the connection to RabbitMQ goes down
+        Process.monitor(conn.pid)
+
+        setup_channel(conn)
+
+      {:error, _} ->
+        # Reconnection loop
+        :timer.sleep(10_000)
+        rabbitmq_connect
+    end
+  end
+
+  defp setup_channel(connection) do
+    {:ok, channel} = AMQP.Channel.open(connection)
     setup_queue(channel)
-    # :ok = Basic.qos(channel, prefetch_count: 10)
+    AMQP.Basic.qos(channel, prefetch_count: 10)
     {:ok, channel}
+  end
+
+
+  # Reconnect on RabbitMQ failure:
+  def handle_info({:DOWN, _, :process, _pid, _reason}, _) do
+    {:ok, chan} = rabbitmq_connect
+    {:noreply, chan}
   end
 
   def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, channel) do
@@ -31,8 +55,9 @@ defmodule Planga.AppSettingsListener do
   def handle_info({:basic_deliver, payload_binary, %{delivery_tag: tag, redelivered: redelivered}}, channel) do
     payload = :erlang.binary_to_term(payload_binary)
     Task.start(fn ->
-      # TODO Actual handling logic here
       IO.puts "Received message: #{inspect(payload)}"
+      update_rails_user(payload)
+      IO.puts "Done updating user!"
     end)
     {:noreply, channel}
   end
@@ -44,4 +69,56 @@ defmodule Planga.AppSettingsListener do
     :ok = AMQP.Queue.bind(channel, queue_name, @exchange_name)
     {:ok, _consumer_tag} = AMQP.Basic.consume(channel, queue_name, nil, no_ack: true)
   end
+
+  @doc """
+  Currently, the Rails system works with 'users', so this is how we are managing individual applications,
+  using the user ID as app name.
+  """
+  defp update_rails_user(user_map) do
+    Planga.Repo.transaction(fn ->
+      user_map["api_credentials"]
+      |> Enum.each(&update_credential/1)
+    end)
+  end
+
+  defp update_credential(api_key_map) do
+    require Logger
+    Planga.Repo.transaction(fn ->
+
+      app =
+        case Planga.Repo.get_by(Planga.Chat.App, name: to_string(api_key_map["public_id"])) do
+          nil ->
+            Logger.info("Creating new app #{api_key_map["public_id"]}")
+            %Planga.Chat.App{name: api_key_map["public_id"]}
+          existing ->
+            Logger.info("Updating existing app #{api_key_map["public_id"]}")
+            existing
+        end
+
+      app =
+        app
+        |> Planga.Chat.App.from_json(api_key_map)
+        |> Planga.Repo.insert_or_update!
+
+
+      api_key_pair =
+        case Planga.Repo.get(Planga.Chat.APIKeyPair, api_key_map["public_id"]) do
+          nil ->
+            Logger.info("Creating new key #{api_key_map["public_id"]}")
+            %Planga.Chat.APIKeyPair{public_id: api_key_map["public_id"]}
+          existing ->
+            Logger.info("Updating existing key #{api_key_map["public_id"]}")
+            existing
+      end
+
+      api_key_map =
+        Map.merge(api_key_map, %{"app_id" => app.id})
+
+      api_key_pair
+      |> Planga.Chat.APIKeyPair.from_json(api_key_map)
+      |> Planga.Repo.insert_or_update!
+      Logger.info("Done with key #{api_key_map["public_id"]}!")
+    end)
+  end
+
 end
