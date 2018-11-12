1c7eb1be33274b961068b0a62eec52c0e66cc021
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-11-12 12:42:14 +0100

    Properly handling Apps that are sent here from the Rails application

--- lib/planga/app_settings_listener.ex
+++ lib/planga/app_settings_listener.ex
@@ -70,11 +70,14 @@ defmodule Planga.AppSettingsListener do
   end
 
   def handle_info({:basic_deliver, payload_binary, %{delivery_tag: tag, redelivered: redelivered}}, channel) do
-    payload = :erlang.binary_to_term(payload_binary, safe: true)
+    payload = :erlang.binary_to_term(payload_binary, [:safe])
     Task.start(fn ->
-      IO.puts "Received message: #{inspect(payload)}"
-      update_rails_user(payload)
-      IO.puts "Done updating user!"
+      Logger.debug fn -> "Received message: #{inspect(payload)}" end
+
+      update_rails_app(payload)
+
+      Logger.debug fn -> "Done updating app!" end
+      Logger.debug fn -> "------------------" end
     end)
     {:noreply, channel}
   end
@@ -100,7 +103,7 @@ defmodule Planga.AppSettingsListener do
   end
 
   @deprecated
-  defp update_credential(api_key_map, app) do
+  defp update_credential(api_key_map) do
     Planga.Repo.transaction(fn ->
 
       app =
@@ -143,19 +146,27 @@ defmodule Planga.AppSettingsListener do
 
   defp update_rails_app(app_map) do
     Planga.Repo.transaction(fn ->
+      app_log_name = "#{app_map["id"]}/`#{app_map["name"]}`"
       app = case Planga.Repo.get_by(Planga.Chat.App, id: app_map["id"]) do
               nil ->
-                Logger.info("Creating new app #{api_key_map["id"]}/#{api_keymap["name"]}")
-                %Planga.Chat.App()
+                Logger.info("Creating new app #{app_log_name}")
+                %Planga.Chat.App{}
               existing ->
-                Logger.info("Updating existing app #{api_key_map["public_id"]}")
+                Logger.info("Updating existing app #{app_log_name}")
                 existing
             end
-            |> Planga.Chat.App.from_hash(app_map)
-            |> Planga.Repo.insert_or_update!()
+
+      app =
+        app
+        |> Planga.Chat.App.from_hash(app_map)
+        |> Planga.Repo.insert_or_update!()
 
       app_map["api_credentials"]
       |> Enum.each(&update_credential2(&1, app))
+
+      Logger.info("Done updating #{app_log_name}")
+
+      :ok
     end)
   end
 
@@ -163,7 +174,7 @@ defmodule Planga.AppSettingsListener do
   defp update_credential2(api_key_map, app) do
     # TODO
 
-    Planga.Repo.transaction(fn ->
+    # Planga.Repo.transaction(fn ->
       api_key_pair =
         case Planga.Repo.get(Planga.Chat.APIKeyPair, api_key_map["public_id"]) do
           nil ->
@@ -181,6 +192,6 @@ defmodule Planga.AppSettingsListener do
       |> Planga.Chat.APIKeyPair.from_json(api_key_map)
       |> Planga.Repo.insert_or_update!
       Logger.info("Done with key #{api_key_map["public_id"]}!")
-    end
+    # end)
   end
 end
