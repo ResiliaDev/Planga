49618fbeb4164dea984b495d7ebd2df28f3983e0
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-03 12:28:50 +0100

    fixes typo in before_script.sh

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,5 +3,5 @@ set -ev # Stop at first error
 
 # Only install webdriven browser if required
 if [ "${TEST_SUITE}" = "--only integration:true"] then
-   - sudo apt-get install chromium-chromedriver
+   sudo apt-get install chromium-chromedriver
 fi
