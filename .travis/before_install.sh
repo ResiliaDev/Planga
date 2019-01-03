091e406bcf58182d6481fd0fa6cea7f8eab0cb8b
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-03 12:24:02 +0100

    only start chromedriver during integration testing, hopefully?

---
+++ .travis/before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ev # Stop at first error
+
+# Only install webdriven browser if required
+if [ "${TEST_SUITE}" = "--only integration:true"] then
+   - sudo apt-get install chromium-chromedriver
+fi
---
+++ .travis/before_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ev # Stop at first error
+
+# Compile elixir project + deps
+MIX_ENV=test mix compile
+
+
+# build front-end assets
+- cd ./assets;
+- yarn
+- ./node_modules/brunch/bin/brunch b -p ;
+- cd ../;
+
+# Start webdriven browser (only when required)
+if [ "${TEST_SUITE}" = "--only integration:true"] then
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    sleep 3 # give xvfb some time to start
+    export PATH=$PATH:/usr/lib/chromium-browser/
+    nohup chromedriver &
+fi
