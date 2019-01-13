5adb2ae9356c6f01639a4ce5e8e35ff23ca4452a
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-13 23:43:30 +0100

    temp long migration logging

--- priv/repo/migrations/20181125152202_fill_conversation_users_in_messages.exs
+++ priv/repo/migrations/20181125152202_fill_conversation_users_in_messages.exs
@@ -10,6 +10,7 @@ defmodule Planga.Repo.Migrations.FillConversationUsersInMessages do
   end
 
   defp fill_conversation_users do
+    require Logger
     Planga.Repo.transaction(fn ->
       EctoMnesia.Table.Stream.new(:message)
       |> Enum.each(fn message_tuple ->
@@ -17,6 +18,7 @@ defmodule Planga.Repo.Migrations.FillConversationUsersInMessages do
         user = Planga.Repo.get!(Planga.Chat.User, message.sender_id)
         conversation = Planga.Repo.get!(Planga.Chat.Conversation, message.conversation_id)
         {:ok, conversation_user} = Planga.Chat.Converse.Persistence.fetch_conversation_user_info(conversation.id, user.id)
+        Logger.info("Updating message #{inspect(message)}")
 
         message
         |> Ecto.Changeset.change(conversation_user_id: conversation_user.id)
