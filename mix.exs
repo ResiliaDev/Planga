02b9e8cc7905b247b6aa80f7fabf7850da1deb69
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-14 07:49:41 +0100

    turn off message filling migration for now, since it takes more than six
    hours to run.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Planga.Mixfile do
   def project do
     [
       app: :planga,
-      version: "0.8.3",
+      version: "0.8.4",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
--- priv/repo/migrations/20181125152202_fill_conversation_users_in_messages.exs
+++ priv/repo/migrations/20181125152202_fill_conversation_users_in_messages.exs
@@ -10,20 +10,20 @@ defmodule Planga.Repo.Migrations.FillConversationUsersInMessages do
   end
 
   defp fill_conversation_users do
-    require Logger
-    Planga.Repo.transaction(fn ->
-      EctoMnesia.Table.Stream.new(:message)
-      |> Enum.each(fn message_tuple ->
-        message = Planga.Repo.get!(Planga.Chat.Message, elem(message_tuple, 1))
-        user = Planga.Repo.get!(Planga.Chat.User, message.sender_id)
-        conversation = Planga.Repo.get!(Planga.Chat.Conversation, message.conversation_id)
-        {:ok, conversation_user} = Planga.Chat.Converse.Persistence.fetch_conversation_user_info(conversation.id, user.id)
-        Logger.info("Updating message #{inspect(message)}")
+    # require Logger
+    # Planga.Repo.transaction(fn ->
+    #   EctoMnesia.Table.Stream.new(:message)
+    #   |> Enum.each(fn message_tuple ->
+    #     message = Planga.Repo.get!(Planga.Chat.Message, elem(message_tuple, 1))
+    #     user = Planga.Repo.get!(Planga.Chat.User, message.sender_id)
+    #     conversation = Planga.Repo.get!(Planga.Chat.Conversation, message.conversation_id)
+    #     {:ok, conversation_user} = Planga.Chat.Converse.Persistence.fetch_conversation_user_info(conversation.id, user.id)
+    #     Logger.info("Updating message #{inspect(message)}")
 
-        message
-        |> Ecto.Changeset.change(conversation_user_id: conversation_user.id)
-        |> Planga.Repo.update
-      end)
-    end)
+    #     message
+    #     |> Ecto.Changeset.change(conversation_user_id: conversation_user.id)
+    #     |> Planga.Repo.update
+    #   end)
+    # end)
   end
 end
