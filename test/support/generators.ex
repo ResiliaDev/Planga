06fd19ac164edb7e19a8a562a083d777df4c3526
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-14 10:49:06 +0100

    fixes compiler warnings in tests

--- test/support/generators.ex
+++ test/support/generators.ex
@@ -18,11 +18,11 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def num_id_generator do
-    pos_integer_generator
+    pos_integer_generator()
   end
 
   def remote_id_generator do
-    [num_id_generator, StreamData.string(:alphanumeric, min_length: 1)]
+    [num_id_generator(), StreamData.string(:alphanumeric, min_length: 1)]
     |> StreamData.one_of()
     |> StreamData.map(&Kernel.to_string/1)
   end
@@ -40,10 +40,9 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def user_generator do
-    ExUnitProperties.gen all id <- num_id_generator,
+    ExUnitProperties.gen all id <- num_id_generator(),
                              app_id <- num_id_generator(),
-                             remote_id <- remote_id_generator,
-                             app_id <- num_id_generator,
+                             remote_id <- remote_id_generator(),
                              name <- StreamData.string(:alphanumeric, min_length: 1) do
       {:ok, res} =
         Planga.Chat.User.new(%{
@@ -57,7 +56,7 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def app_generator do
-    ExUnitProperties.gen all id <- num_id_generator,
+    ExUnitProperties.gen all id <- num_id_generator(),
                              name <- StreamData.string(:printable, min_length: 1) do
       {:ok, res} = Planga.Chat.App.new(name: name)
 
@@ -68,9 +67,9 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def conversation_generator do
-    ExUnitProperties.gen all id <- num_id_generator,
-                             remote_id <- remote_id_generator,
-                             app_id <- num_id_generator do
+    ExUnitProperties.gen all id <- num_id_generator(),
+                             remote_id <- remote_id_generator(),
+                             app_id <- num_id_generator() do
       {:ok, res} =
         Planga.Chat.Conversation.new(%{
           app_id: app_id,
@@ -82,8 +81,8 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def filled_conversation_generator do
-    ExUnitProperties.gen all app <- app_generator,
-                             conversation <- conversation_generator do
+    ExUnitProperties.gen all app <- app_generator(),
+                             conversation <- conversation_generator() do
       conversation = put_in(conversation.app_id, app.id)
       conversation = put_in(conversation.app, app)
 
@@ -92,10 +91,10 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def conversation_user_generator do
-    ExUnitProperties.gen all id <- num_id_generator,
-                             conversation_id <- num_id_generator,
-                             user_id <- num_id_generator,
-                             role <- role_generator do
+    ExUnitProperties.gen all id <- num_id_generator(),
+                             conversation_id <- num_id_generator(),
+                             user_id <- num_id_generator(),
+                             role <- role_generator() do
       {:ok, res} =
         Planga.Chat.ConversationUser.new(%{
           conversation_id: conversation_id,
@@ -109,9 +108,9 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def filled_conversation_user_generator do
-    ExUnitProperties.gen all conversation <- filled_conversation_generator,
-                             user <- user_generator,
-                             conversation_user <- conversation_user_generator do
+    ExUnitProperties.gen all conversation <- filled_conversation_generator(),
+                             user <- user_generator(),
+                             conversation_user <- conversation_user_generator() do
       conversation_user = put_in(conversation_user.user_id, user.id)
       conversation_user = put_in(conversation_user.user, user)
 
@@ -120,6 +119,8 @@ defmodule Planga.Test.Support.Generators do
 
       conversation_user = put_in(conversation_user.user.app_id, conversation.app.id)
       conversation_user = put_in(conversation_user.user.app, conversation.app)
+
+      conversation_user
     end
   end
 
@@ -127,10 +128,10 @@ defmodule Planga.Test.Support.Generators do
     ExUnitProperties.gen all content <- StreamData.string(:printable),
                              content != "",
                              String.length(content) < 1024,
-                             sender_id <- num_id_generator,
-                             conversation_id <- num_id_generator,
-                             conversation_user_id <- num_id_generator,
-                             inserted_at <- datetime_generator do
+                             sender_id <- num_id_generator(),
+                             conversation_id <- num_id_generator(),
+                             conversation_user_id <- num_id_generator(),
+                             inserted_at <- datetime_generator() do
       {:ok, res} =
         Planga.Chat.Message.new(%{
           content: content,
@@ -145,9 +146,8 @@ defmodule Planga.Test.Support.Generators do
   end
 
   def filled_message_generator do
-    ExUnitProperties.gen all conversation_user <- filled_conversation_user_generator,
-                             conversation <- conversation_generator,
-                             message <- message_generator do
+    ExUnitProperties.gen all conversation_user <- filled_conversation_user_generator(),
+                             message <- message_generator() do
       message = put_in(message.conversation_user_id, conversation_user.id)
       message = put_in(message.conversation_user, conversation_user)
 
@@ -156,12 +156,14 @@ defmodule Planga.Test.Support.Generators do
 
       message = put_in(message.sender_id, conversation_user.user.id)
       message = put_in(message.sender, conversation_user.user)
+
+      message
     end
   end
 
   def deleted_message_generator do
-    ExUnitProperties.gen all message <- message_generator,
-                             deleted_at <- datetime_generator,
+    ExUnitProperties.gen all message <- message_generator(),
+                             deleted_at <- datetime_generator(),
                              deleted_at > message.inserted_at do
       put_in(message.deleted_at, deleted_at)
     end
